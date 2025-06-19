7ddb99b2a163ace9f59086a963866689e8e0fd1f
eric matutu <ericmatutu125@gmail.com>
2025-06-20 01:39:53 +0300

    added 3 route

---
+++ task_manager_client/src/pages/About.jsx
@@ -0,0 +1,14 @@
+
+
+function About() {
+
+
+    return(
+        <>
+        <h1>Mocha smart Task manager</h1>
+        <p>This app is built with React and love. 🧠🔥</p>;
+        </>
+    )
+}
+
+export default About;
---
+++ task_manager_client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+function Home() {
+
+    return(
+        <div>
+            <h1>Smart Task Manager</h1>
+            <p>plan you're day ahead</p>
+
+            <button>Get started</button>
+        </div>
+    );
+}
+
+export default Home;
